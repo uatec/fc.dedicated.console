6f57cbfaa014966d2ea2215de0fa811e4708ae09
Robert Stiff <uatecuk@gmail.com>
2016-05-20 19:56:14 +0100

    pipe environment variables up to the UI for configuration.

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
 var express = require('express');
 var app = express();
+var _ = require('lodash');
+
+
+
+
+var envVars = '<script>GLOBAL = {}; GLOBAL.env=' + JSON.stringify(_.pick(process.env, [
+    'auth0clientid',
+    'auth0domain',
+    'API_URL'
+])) + '</script>';
+
+
+app.get('/', function (request, response) {
+    response.send(
+        "<html>" +
+            "<head>" +
+                envVars +
+                "<style>* {font-family: Roboto, sans-serif} html, body { margin: 0px }</style>" +
+            "</head>" +
+            "<body>" +
+                "<div id='content' />" +
+                "<script src='/js/bundle.js'></script>" +
+            "</body>" +
+        "</html>");
+});
 
 app.use(express.static('public'));
 
